0baa15768687f5d04e05de8ff8e9b9e8a7b060eb
Александр Умнов <alexmac@Aleksandrs-MacBook-Air.local>
2021-10-09 11:27:30 +0300

    heroku adapted

--- cuddle_bot/utils/access_token.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def get_access_token() -> str:
-    try:
-        return os.environ['ACCESS_TOKEN']
-    except KeyError as e:
-        raise RuntimeError("You need to set up ACCESS_TOKEN env var")
--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 from utils.access_token import get_access_token
 import logging
+import os
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+
+PORT = int(os.environ.get('PORT', 5000))
+APPLICATION_NAME = f'https://{os.environ.get("HEROKU_APP_NAME", "powerful-everglades-39634.")}.herokuapp.com/'
+HEROKU_TOKEN = os.environ.get("HEROKU_TOKEN", None)
+TG_ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -12,7 +18,7 @@ def start(update, context):
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"You said {update.message.text}")
 
-updater = Updater(token=get_access_token(), use_context=True)
+updater = Updater(token=TG_ACCESS_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -25,4 +31,7 @@ echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 
 
-updater.start_polling()
+updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=HEROKU_TOKEN)
+updater.bot.setWebhook(APPLICATION_NAME + HEROKU_TOKEN)
